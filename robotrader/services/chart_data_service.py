9cf3d46a21babe0d0751e3e4748db779e3e89cba
Rafael <rafaeldias85@live.com>
2024-01-06 00:43:09 -0300

    lib nova

--- robotrader/services/chart_data_service.py
+++ robotrader/services/chart_data_service.py
@@ -1,22 +1,40 @@
-import yfinance as yf
-import logging
+import MetaTrader5 as mt5
 import pandas as pd
+import logging
 
 class ChartDataService:
-    def fetch_time_series_intraday(self, symbol, interval):
-        interval_map = {
-            "1min": "1m",
-            "5min": "5m",
-            "15min": "15m",
-            "1h": "1h"
-        }
-        forex_symbol = f"{symbol}=X"
+    def __init__(self):
+        pass
+
+    def connect(self):
+        if not mt5.initialize():
+            logging.error("Erro ao conectar ao servidor MetaTrader 5")
+            return False
+        return True
+
+    def disconnect(self):
+        mt5.shutdown()
+
+    def fetch_real_time_data(self, symbol):
+        forex_symbol = f"{symbol}"
         try:
-            # Considerar aumentar o período se necessário
-            data = yf.download(forex_symbol, period="3mo", interval=interval_map[interval])
-            if not isinstance(data.index, pd.DatetimeIndex):
-                data.index = pd.to_datetime(data.index)
-            return data
+            symbol_info = mt5.symbol_info(forex_symbol)
+            if symbol_info is None:
+                logging.error(f"{forex_symbol} não encontrado, erro {mt5.last_error()}")
+                return None
+
+            if not symbol_info.visible:
+                logging.error(f"{forex_symbol} não é visível, tentando mudar...")
+                if not mt5.symbol_select(forex_symbol, True):
+                    logging.error(f"symbol_select({forex_symbol}) falhou, erro {mt5.last_error()}")
+                    return None
+
+            ticks = mt5.copy_ticks_from(forex_symbol, mt5.TIME_CURRENT, 1000, mt5.COPY_TICKS_ALL)
+            data_frame = pd.DataFrame(ticks)
+            data_frame['time'] = pd.to_datetime(data_frame['time'], unit='s')
+            return data_frame
         except Exception as e:
             logging.error(f"Erro ao buscar dados para {forex_symbol}: {e}")
             raise
+        finally:
+            self.disconnect()
